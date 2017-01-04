34fcf020ba5924ad9cd370812502b3da1b1faca9
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2017-01-04 13:23:50 +0300

    Add odd-even game

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,10 @@
 // @flow
-import readlineSync from 'readline-sync';
+import oddEven from './games/odd_even';
 
 const cli = () => {
   console.log('Welcome to the Brain Games!');
 
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  oddEven();
 };
 
 export default cli;
