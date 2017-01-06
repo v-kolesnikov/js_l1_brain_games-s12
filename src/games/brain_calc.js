74fcaedcbd7705d91210ca963ac77fc39903b42a
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2017-01-07 01:01:25 +0300

    Add Brain Calc game

---
+++ src/games/brain_calc.js
@@ -0,0 +1,27 @@
+// @flow
+
+import game from '..';
+import * as Utils from '../utils';
+
+const description = 'What is the result of the expression?';
+
+const operations = (): [string, Function][] =>
+  [['+', (a, b) => a + b],
+   ['-', (a, b) => a - b],
+   ['*', (a, b) => a * b],
+   ['/', (a, b) => a / b]];
+
+const nextQuestion = (): { question: string, answer: string } => {
+  const operand1 = Utils.getRandomNumber(0, 100);
+  const operand2 = Utils.getRandomNumber(0, 100);
+  const [op, fn] = operations()[Utils.getRandomNumber(0, 3)];
+  const question = [operand1, op, operand2].join(' ');
+  const answer = String(fn(operand1, operand2));
+
+  return { question, answer };
+};
+
+const calcGame = () =>
+  game(description, nextQuestion);
+
+export default calcGame;
